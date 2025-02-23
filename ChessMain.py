e562ff5d9b01335d14379f00ffe6b1b044afbd00
Shiv10 <shivanshsharma2012@gmail.com>
2025-02-23 15:47:28 +0530

    feat: add functionality to load images

--- ChessMain.py
+++ ChessMain.py
@@ -2,5 +2,22 @@
 This is our driver file. This will handle user input and display the current GameState
 """
 
+import pygame as p
+import ChessEngine
+import os
 
-        
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+'''
+initialize global dictionary of images. Load images only once at start of game and save in memory
+'''
+def loadImages():
+    pieces = ["wp", "wR", "wN", "wB", "wQ", "wK", "bp", "bR", "bN", "bB", "bQ", "bK"]
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load(os.path.join("images", piece + ".png")), (SQ_SIZE, SQ_SIZE))
+
+
